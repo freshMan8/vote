8acf8f86c012191e5d6f442715f77d93bdf63416
liufan <3832@winnermedical.com>
2023-08-09 17:33:35 +0800

    代码基座提交

---
+++ src/main/java/com/tencent/wxcloudrun/dto/ActivityDetailRequest.java
@@ -0,0 +1,15 @@
+package com.tencent.wxcloudrun.dto;
+
+import lombok.Data;
+
+/**
+ * TO-DO
+ *
+ * @author 3832
+ * @date 2023/8/9 17:10
+ */
+@Data
+public class ActivityDetailRequest {
+
+    private Long activityId;
+}
